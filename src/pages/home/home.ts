0d1a98122b7c616dd17c13e9ee2e7d5dcedf327e
unknown <DAM217@ACARBALLEIRA.LOCAL>
2018-11-16 13:47:58 +0100

    final

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,13 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  title = 'Tour of Heroes';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado','Joel'];
+  myHero = this.heroes[0]+" and "+this.heroes[3];
+
 
   constructor(public navCtrl: NavController) {
 
   }
-
 }
